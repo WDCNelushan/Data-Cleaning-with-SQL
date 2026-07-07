380275adcce53adeee1a56148ae5e46564bb2b95
WDCNelushan <nelushanwickramarachchi2002@gmail.com>
2026-07-07 15:40:18 +0530

    Standardize the data set

--- layoffs_cleaned.sql
+++ layoffs_cleaned.sql
@@ -57,4 +57,35 @@ select *
 from layoffs_staging2
 where row_num > 1;
 
+-- 4. Standardize data
+
+select company, TRIM(company)
+from layoffs_staging2;
+
+-- Remove trailing spaces 
+UPDATE layoffs_staging2 SET company = TRIM(company);
+
+select distinct industry
+from layoffs_staging2
+order by industry;
+
+-- Fix inconsistent categories (e.g., "Crypto Currency" -> "Crypto")
+UPDATE layoffs_staging2 SET industry = 'Crypto' WHERE industry LIKE 'Crypto%';
+
+UPDATE layoffs_staging2 SET country = "United States" WHERE country LIKE 'United States%';
+-- UPDATE layoffs_staging2 SET country = TRIM(TRAILING '.' from country) WHERE country LIKE 'United States%';
+
+select distinct country
+from layoffs_staging2;
+
+-- convert date column into date formate
+select `date`, str_to_date(`date`,'%m/%d/%Y')
+from layoffs_staging2;
+
+UPDATE layoffs_staging2 SET `date` = str_to_date(`date`,'%m/%d/%Y');
+
+ALTER TABLE layoffs_staging2 MODIFY COLUMN `date` DATE;
+
+select *
+from layoffs_staging2;
 
