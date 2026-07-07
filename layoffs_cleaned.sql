2de813babd8485c41ee2a953cf9b0d3e16be42d8
WDCNelushan <nelushanwickramarachchi2002@gmail.com>
2026-07-07 12:45:22 +0530

    Removed duplicates from the data set

--- layoffs_cleaned.sql
+++ layoffs_cleaned.sql
@@ -9,3 +9,52 @@ INSERT INTO layoffs_staging SELECT * FROM layoffs;
 
 select *
 from layoffs_staging;
+
+-- 3. Remove Duplicates
+
+WITH duplicate_cte AS (
+SELECT *, 
+    ROW_NUMBER() OVER(
+        PARTITION BY company, location, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions
+    ) AS row_num
+    FROM layoffs_staging
+)
+select *
+from duplicate_cte
+where row_num > 1;
+
+CREATE TABLE `layoffs_staging2` (
+  `company` text,
+  `location` text,
+  `industry` text,
+  `total_laid_off` int DEFAULT NULL,
+  `percentage_laid_off` text,
+  `date` text,
+  `stage` text,
+  `country` text,
+  `funds_raised_millions` int DEFAULT NULL,
+  `row_num` INT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+select *
+from layoffs_staging2;
+
+INSERT INTO layoffs_staging2
+SELECT *, ROW_NUMBER() OVER(
+        PARTITION BY company, location, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions
+    ) AS row_num
+    FROM layoffs_staging;
+
+select *
+from layoffs_staging2
+where row_num > 1;
+
+delete 
+from layoffs_staging2
+where row_num > 1;
+
+select *
+from layoffs_staging2
+where row_num > 1;
+
+
